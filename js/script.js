83dae952e6a8825ab9a4a7b9f3be0e9d1175ed7a
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-06-05 20:45:28 +0200

    working on thumbnails bar

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ createApp({
         images: [
             {
                 image: 'img/01.webp',
-                title: 'Marvel\'s Spiderman Miles Morale',
+                title: 'Spiderman Miles Morale',
                 text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
             }, 
             {
